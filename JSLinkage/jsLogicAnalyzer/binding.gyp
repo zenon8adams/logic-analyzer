81e653e02b00625e3c6df59851a2ceaecbc7b154
Meekness Adesina <zenon8adams@gmail.com>
2023-04-09 16:16:42 +0100

    Updated build script

--- JSLinkage/jsLogicAnalyzer/binding.gyp
+++ JSLinkage/jsLogicAnalyzer/binding.gyp
@@ -3,13 +3,13 @@
     {
       "target_name":    "logic_analyzer",
 	  "include_dirs": [ "<!(node -e \"require('nan')\")", "../../include", "../utility/builder"],
-	  "library_dirs": [ "<!(echo $PWD)/", "<!(echo $PWD)/build"],
+	  "library_dirs": [ "<!(echo $PWD)/", "<!(echo $PWD)/build_deps"],
       "sources":      [  "../utility/builder/json-builder.cpp", "jsLogicAnalyzer.cpp"],
       "cflags_cc":    [ "-std=c++17", "-fexceptions", "-fPIC"],
       "link_settings": {
           "libraries": [
             "-ljs__conn -lanalyzer",
-            "-Wl,-rpath=./build"
+            "-Wl,-rpath=./build_deps"
 
           ],
           "library_dirs": [
--- JSLinkage/jsLogicAnalyzer/main.js
+++ JSLinkage/jsLogicAnalyzer/main.js
@@ -1,12 +1,11 @@
 "use strict";
 
 const EventEmitter = require( 'events');
-const path = require( "path");
+const path         = require( "path");
 
 const make_analyzer = function ( opts)
 {
-    const addon_path = path.join( __dirname, "build/Release/logic_analyzer");
-    const worker = require( addon_path);
+    const worker = require('bindings')('logic_analyzer');
 
     const emitter = new EventEmitter();
     worker.StreamingWorker( function ( key, value) {
@@ -20,13 +19,29 @@ const make_analyzer = function ( opts)
     return emitter;
 };
 
-const addon = make_analyzer( { input: "(((A & B) | D | C) => ((A ^ B) & ~E | C)) <=> (((~C & ~F) & ~A | ~G) => ((A | ~B) | A & ~H))"});
+const addon = make_analyzer({ input: "A & B | C & D"});//" <=> (((~C & ~F) & ~A | ~G) => ((A | ~B) | A & ~H))"});
+
+let headers = null;
+let rows    = [];
+let table   = [];
 
 addon.on( 'packet', ( packet) => {
-    const result = JSON.parse( packet);
-    if( result.headers != null)
-        console.log( result);
+    const entry = JSON.parse( packet);
+    if( entry.headers != null)
+    {
+        headers = entry.headers;
+    }
     else
-        console.log( packet);
+    {
+        rows.push( entry.rows);
+        const row = {};
+        entry.rows.forEach(( each, index) => row[ headers[ index]] = each); 
+        table.push( row);
+    }
 });
+
 addon.on( 'error', ( message) => console.log( message));
+
+addon.on( 'close', () => {
+    console.table( table, headers);
+});
