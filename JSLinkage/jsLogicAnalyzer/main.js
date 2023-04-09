2805d9b53f15926bdc671105fd0a544e3a00c2b4
Meekness Adesina <zenon8adams@gmail.com>
2023-04-10 00:41:53 +0100

    Extended js example

--- JSLinkage/jsLogicAnalyzer/main.js
+++ JSLinkage/jsLogicAnalyzer/main.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const EventEmitter = require( 'events');
-const path         = require( "path");
+const path         = require( 'path');
+const readline     = require( 'readline');
 
 const make_analyzer = function ( opts)
 {
@@ -19,29 +20,60 @@ const make_analyzer = function ( opts)
     return emitter;
 };
 
-const addon = make_analyzer({ input: "A & B | C & D"});//" <=> (((~C & ~F) & ~A | ~G) => ((A | ~B) | A & ~H))"});
+const tabulate = function( text)
+{
+    let headers = null;
+    let rows    = [];
+    let table   = [];
 
-let headers = null;
-let rows    = [];
-let table   = [];
+    const addon = make_analyzer({ input: text});
+    addon.on( 'packet', ( packet) => {
+        const entry = JSON.parse( packet);
+        if( entry.headers != null)
+        {
+            headers = entry.headers;
+        }
+        else
+        {
+            rows.push( entry.rows);
+            const row = {};
+            entry.rows.forEach(( each, index) => row[ headers[ index]] = each); 
+            table.push( row);
+        }
+    });
 
-addon.on( 'packet', ( packet) => {
-    const entry = JSON.parse( packet);
-    if( entry.headers != null)
-    {
-        headers = entry.headers;
-    }
-    else
-    {
-        rows.push( entry.rows);
-        const row = {};
-        entry.rows.forEach(( each, index) => row[ headers[ index]] = each); 
-        table.push( row);
-    }
+    addon.on( 'error', ( message) => console.log( message));
+
+    addon.on( 'close', () => {
+        console.table( table, headers);
+        headers = null;
+        rows = [];
+        table = [];
+        controller
+            .question( 
+                "Solve another(yes/no/Y/N/)? ", reply => {
+                if([ "yes", "y"].includes( reply.toLowerCase()))
+                    main();
+                else
+                    process.exit( 0);
+            });
+    });
+};
+
+const controller = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-addon.on( 'error', ( message) => console.log( message));
+const main = function()
+{
+    controller.question( 
+        "KEY: XOR( ^, @ ), OR( |, \/ ), AND( &, /\ )" +
+        ", NOT( ~, ! ), IF( =>, -> ), IFF( <=> )" +
+        "\nEnter a logical expression: ", query => {
+        if( query.length !== 0)
+            tabulate( query);
+    });
+}
 
-addon.on( 'close', () => {
-    console.table( table, headers);
-});
+main();
